89c39cd05faa9580e22b683a665b0f60cf7bcbd3
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-20 12:08:01 +0530

    add basic tweetlist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
-import Tweet from "./components/Tweet";
+import TweetList from "./components/TweetList";
+
+const initialDummyTweets = [
+  {
+    id: 0,
+    content: "Hello, React!",
+    likeCount: 10,
+  },
+  {
+    id: 1,
+    content: "Bye, React!",
+    likeCount: 5,
+  },
+  {
+    id: 2,
+    content: "Hi, React! Again",
+    likeCount: 2,
+  },
+];
+
 function App() {
   return (
     <div>
-      <Tweet content="Hello World" likeCount={10} />
+      <TweetList tweets={initialDummyTweets} />
     </div>
   );
 }
